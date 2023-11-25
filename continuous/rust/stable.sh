9986e0c63b23224ca26dbf304937ffc5a926819b
Willy Micieli <micieli@vivaldi.net>
2023-11-25 18:32:18 +0100

    add git show log

--- continuous/rust/stable.sh
+++ continuous/rust/stable.sh
@@ -3,4 +3,6 @@ echo stable
 rustup default stable
 sudo apt-get install git -y
 git clone https://github.com/taishingi/continuous-testing && cd continuous-testing || exit 1
+git log -1 
+sleep 10
 cargo build || exit 1
