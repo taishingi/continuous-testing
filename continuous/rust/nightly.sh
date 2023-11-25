9b86fd81435b20cb78a87c12eed2a79b8f0b8a7d
Willy Micieli <micieli@vivaldi.net>
2023-11-25 18:29:17 +0100

    refractor app continuous script

--- continuous/rust/nightly.sh
+++ continuous/rust/nightly.sh
@@ -1,7 +1,5 @@
 echo nightly
-
 rustup default nightly 
 sudo apt-get install git -y
-git clone https://github.com/taishingi/continuous-testing
-cd continuous-testing
-cargo build
+git clone https://github.com/taishingi/continuous-testing && cd continuous-testing || exit 1
+cargo build || exit 1
--- continuous/rust/stable.sh
+++ continuous/rust/stable.sh
@@ -1,8 +1,6 @@
 echo stable
 
 rustup default stable
-
 sudo apt-get install git -y
-git clone https://github.com/taishingi/continuous-testing
-cd continuous-testing
-cargo build
+git clone https://github.com/taishingi/continuous-testing && cd continuous-testing || exit 1
+cargo build || exit 1
