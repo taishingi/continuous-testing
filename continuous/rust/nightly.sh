623959915de1d15dc390cbac8ba33e159ee0942c
Willy Micieli <micieli@vivaldi.net>
2023-11-26 11:55:32 +0100

    update version

--- continuous/rust/nightly.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo nightly
-rustup default nightly
-sudo apt-get install git -y
-git clone https://github.com/taishingi/continuous-testing && cd continuous-testing || exit 1
-git log -1 || exit 1
-sleep 10
-cargo build || exit 1
