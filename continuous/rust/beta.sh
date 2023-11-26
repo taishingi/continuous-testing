3fdf72015afceb2091177d1ce552892a66417f69
Willy Micieli <micieli@vivaldi.net>
2023-11-26 09:42:22 +0100

    add shebang

--- continuous/rust/beta.sh
+++ continuous/rust/beta.sh
@@ -1,8 +1,10 @@
-echo beta
+#!/bin/bash
+
+echo "beta"
 
 rustup default beta
 sudo apt-get install git -y
 git clone https://github.com/taishingi/continuous-testing && cd continuous-testing || exit 1
-git log -1 || exit 1
+git log -1 || exit
 sleep 10
 cargo build || exit 1
--- continuous/rust/stable.sh
+++ continuous/rust/stable.sh
@@ -1,8 +1,9 @@
-echo stable
+#!/bin/bash
 
+echo stable
 rustup default stable
 sudo apt-get install git -y
 git clone https://github.com/taishingi/continuous-testing && cd continuous-testing || exit 1
-git log -1 
+git log -1 || exit 1
 sleep 10
 cargo build || exit 1
