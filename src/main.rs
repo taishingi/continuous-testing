93ff06cdff479448c21a8e22fc55ec1d39839e1d
Willy Micieli <micieli@vivaldi.net>
2024-01-19 09:12:03 +0100

    add nofime depedencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use notifme::Notification;
+use std::env::current_dir;
+use std::fs::File;
 use std::{
     fs,
     path::Path,
@@ -8,9 +11,28 @@ const HOOK: &str = ".git/hooks/post-commit";
 const HOOK_DIR: &str = ".git/hooks";
 const TMP_DIR: &str = "/tmp/continuous-testing";
 const CONTINUOUS: &str = "continuous";
-const TMP_HOOK: &str = "/tmp/continuous-testing/post-commit";
-const ICON_DIR: &str = ".icon";
+const ICON_DIR: &str = ".icons";
 
+fn init_hook() {
+    assert!(Command::new("wget")
+        .arg("-q")
+        .arg("https://raw.githubusercontent.com/taishingi/continuous-testing/master/post-commit")
+        .current_dir(HOOK_DIR)
+        .spawn()
+        .expect("Failed to upgrade the script")
+        .wait()
+        .expect("")
+        .success());
+    assert!(Command::new("chmod")
+        .arg("+x")
+        .arg(HOOK)
+        .current_dir(".")
+        .spawn()
+        .expect("failed to run chmod")
+        .wait()
+        .expect("")
+        .success());
+}
 fn help(args: &[String]) -> i32 {
     println!("{}              : Run the hook", args[0]);
     println!("{} --help       : Display help", args[0]);
@@ -18,12 +40,39 @@ fn help(args: &[String]) -> i32 {
     println!("{} upgrade      : Upgrade the hook file", args[0]);
     1
 }
+fn commit() -> String {
+    if Path::new("commit").is_file() {
+        fs::remove_file("commit").expect("failed to remove the file");
+    }
+    let f = File::create("commit").expect("failed to create the file");
+    assert!(Command::new("git")
+        .arg("log")
+        .arg("-1")
+        .arg("--pretty=%s")
+        .stdout(f)
+        .current_dir(".")
+        .spawn()
+        .expect("failed to get last commit")
+        .wait()
+        .expect("")
+        .success());
 
+    fs::read_to_string("commit").expect("failed to read the commit file")
+}
+fn send(summary: &str, body: &str) {
+    let icon = format!("{}/{ICON_DIR}/continuous.png", env!("HOME"));
+
+    assert!(Notification::new()
+        .app("Continuous testing")
+        .icon(icon.as_str())
+        .summary(summary)
+        .body(body)
+        .send());
+}
 fn init(args: &[String]) -> i32 {
     if args.is_empty() {
         exit(help(args));
     }
-
     if Path::new(TMP_DIR).exists() {
         assert!(Command::new("git")
             .arg("pull")
@@ -46,23 +95,20 @@ fn init(args: &[String]) -> i32 {
             .expect("")
             .success());
     }
-
-    if !Path::new(ICON_DIR).exists() {
-        fs::create_dir(ICON_DIR).expect("failed to create the .icon directory");
-        assert!(fs::copy("/tmp/continuous-testing/.icon/notif.png", ".icon/notif.png").is_ok());
+    let icons = format!("{}/{}", env!("HOME"), ICON_DIR);
+    if !Path::new(icons.as_str()).exists() {
+        fs::create_dir(icons.as_str()).expect("failed to create the icon directory");
     }
 
-    assert!(fs::copy(TMP_HOOK, HOOK).is_ok());
+    if !Path::new(format!("{icons}/continuous.png").as_str()).exists() {
+        assert!(fs::copy(
+            "/tmp/continuous-testing/.icon/notif.png",
+            format!("{icons}/continuous.png").as_str()
+        )
+        .is_ok());
+    }
 
-    assert!(Command::new("chmod")
-        .arg("+x")
-        .arg(HOOK)
-        .current_dir(".")
-        .spawn()
-        .expect("failed to run chmod")
-        .wait()
-        .expect("")
-        .success());
+    init_hook();
 
     if !Path::new(CONTINUOUS).exists() {
         assert!(Command::new("bash")
@@ -91,9 +137,12 @@ fn main() -> ExitCode {
             .expect("")
             .success()
         {
+            send(commit().as_str(), "All test passes");
             exit(0);
+        } else {
+            send(commit().as_str(), "Tests fail");
+            exit(1);
         }
-        exit(1);
     }
     if args.len() == 2 {
         if args.get(1).expect("Failed to get argument").eq("init") {
@@ -101,7 +150,18 @@ fn main() -> ExitCode {
                 println!("Already initialized");
                 exit(0);
             }
-            exit(init(&args));
+            if init(&args).eq(&0) {
+                send(
+                    current_dir()
+                        .expect("")
+                        .as_path()
+                        .to_str()
+                        .expect("failed to get current dir"),
+                    "Is now tracked by continuous testing",
+                );
+                exit(0);
+            }
+            exit(1);
         } else if !Path::new(".git").exists() {
             assert!(Command::new("git")
                 .arg("init")
@@ -110,30 +170,31 @@ fn main() -> ExitCode {
                 .wait()
                 .expect("")
                 .success());
-            exit(init(&args));
+            if init(&args).eq(&0) {
+                send(
+                    current_dir()
+                        .expect("")
+                        .as_path()
+                        .to_str()
+                        .expect("failed to get current dir"),
+                    "Is now tracked by continuous testing",
+                );
+                exit(0);
+            }
+            send(
+                current_dir()
+                    .expect("")
+                    .as_path()
+                    .to_str()
+                    .expect("failed to get current dir"),
+                "Failed to track the repository",
+            );
+            exit(1);
         } else if args.get(1).expect("failed to get argument").eq("upgrade")
             && Path::new(HOOK).exists()
         {
             fs::remove_file(HOOK).expect("failed to remove file");
-            assert!(Command::new("wget")
-                .arg("-q")
-                .arg("https://raw.githubusercontent.com/taishingi/continuous-testing/master/post-commit")
-                .current_dir(HOOK_DIR)
-                .spawn()
-                .expect("Failed to upgrade the script")
-                .wait()
-                .expect("")
-                .success());
-            assert!(Command::new("chmod")
-                .arg("+x")
-                .arg(HOOK)
-                .current_dir(".")
-                .spawn()
-                .expect("failed to run chmod")
-                .wait()
-                .expect("")
-                .success());
-
+            init_hook();
             exit(0);
         } else if !Path::new(HOOK).exists()
             && args.get(1).expect("failed to get argument").eq("upgrade")
